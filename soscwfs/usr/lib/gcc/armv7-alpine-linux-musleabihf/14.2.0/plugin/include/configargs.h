ac113268c8968d5e26555dd75db63bccc2f388ed
StringManolo <manuelvarelacaldas@gmail.com>
2025-09-13 18:15:50 +0200

    Crunch and add gcc/g++ built essentials

---
+++ soscwfs/usr/lib/gcc/armv7-alpine-linux-musleabihf/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/buildozer/aports/main/gcc/src/gcc-14.2.0/configure --prefix=/usr --mandir=/usr/share/man --infodir=/usr/share/info --build=armv7-alpine-linux-musleabihf --host=armv7-alpine-linux-musleabihf --target=armv7-alpine-linux-musleabihf --enable-checking=release --disable-cet --disable-fixed-point --disable-libstdcxx-pch --disable-multilib --disable-nls --disable-werror --disable-symvers --enable-__cxa_atexit --enable-default-pie --enable-default-ssp --enable-languages=c,c++,objc,go,fortran,ada --enable-link-serialization=2 --enable-linker-build-id --with-arch=armv7-a --with-tune=generic-armv7-a --with-fpu=vfpv3-d16 --with-float=hard --with-abi=aapcs-linux --with-mode=thumb --disable-libquadmath --disable-libssp --disable-libsanitizer --enable-shared --enable-threads --enable-tls --disable-libitm --with-bugurl=https://gitlab.alpinelinux.org/alpine/aports/-/issues --with-system-zlib --with-linker-hash-style=gnu --with-pkgversion='Alpine 14.2.0'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "arm10e" }, { "arch", "armv7-a" }, { "tune", "generic-armv7-a" }, { "float", "hard" }, { "mode", "thumb" }, { "fpu", "vfpv3-d16" }, { "tls", "gnu" } };
